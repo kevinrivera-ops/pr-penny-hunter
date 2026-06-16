e81a26f4dfa7a3d063dd48674b83898eedf5cbb9
kevinrivera-ops <kevin.rivera.gonz@gmail.com>
2026-06-16 09:59:15 -0400

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,76 +1,24 @@
 import requests
-import json
 import os
-from bs4 import BeautifulSoup
 
 
-TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
-TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
 
-def send_alert(message):
-
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-
-    requests.post(
-        url,
-        data={
-            "chat_id": TELEGRAM_CHAT_ID,
-            "text": message
-        }
-    )
+message = "🚨 TEST SUCCESS - Penny Hunter is connected!"
 
 
-def check_product(product):
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-    headers = {
-        "User-Agent": "Mozilla/5.0"
+
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
     }
-
-    response = requests.get(
-        product["url"],
-        headers=headers
-    )
-
-    page = response.text
-
-    soup = BeautifulSoup(
-        page,
-        "html.parser"
-    )
-
-    text = soup.get_text()
-
-    if True:
-
-        send_alert(
-            f"""
-🚨 PENNY DEAL FOUND
-
-Item:
-{product['name']}
-
-Store:
-{product['store']}
-
-Link:
-{product['url']}
-"""
-        )
+)
 
 
-with open("watchlist.json") as file:
-    products = json.load(file)
-
-
-for product in products:
-
-    try:
-        check_product(product)
-
-    except Exception as error:
-
-        print(
-            product["name"],
-            error
-        )
+print(response.text)
